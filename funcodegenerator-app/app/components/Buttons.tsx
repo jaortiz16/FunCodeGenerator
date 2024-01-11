20a712990d3e3d8aac1283aee47751a1622d6c5f
Angel Ernesto Aguirre Prieto <65724204+aeaguirre2@users.noreply.github.com>
2024-01-11 14:01:46 -0500

    Button
    Creacion componente button

---
+++ funcodegenerator-app/app/components/Buttons.tsx
@@ -0,0 +1,8 @@
+
+export function Buttons() {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Boton de prueba
+        </button>
+    )
+}
