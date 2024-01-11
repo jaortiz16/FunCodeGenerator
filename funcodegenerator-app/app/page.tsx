27c7be22ab9bb5fb54390df8e53a0c1d93a7619a
Andres Ortiz <63428778+jaortiz16@users.noreply.github.com>
2024-01-11 13:32:52 -0500

    Update page.tsx
    update

--- funcodegenerator-app/app/page.tsx
+++ funcodegenerator-app/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
+import { Canvas } from './components/Canvas'
+import {Buttons} from './components/Buttons'
 import {Tittle} from './components/Tittle'
 export default function Home() {
   return (
     <main >
+      <div>
       <Tittle />
+    <Canvas />
+    <Buttons />
+    
+      </div>
+     
     </main>
   )
 }
