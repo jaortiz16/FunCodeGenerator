509a786f90a497a0b7d90d2bab24cd8b32c7590b
Andres Ortiz <63428778+jaortiz16@users.noreply.github.com>
2024-02-04 16:18:26 -0500

    Update app.js

--- funcodegenerator-site/backend/funcodegenerator-api-auth/src/app.js
+++ funcodegenerator-site/backend/funcodegenerator-api-auth/src/app.js
@@ -17,11 +17,12 @@ app.set("json spaces", 4);
 // Middlewares
 app.use(
   cors({
-   origin: "https://fun-code-generator-7jkh.vercel.app/",
-   methods: "GET,HEAD,PUT,PATCH,POST",
-   credentials: true, 
+    origin: ["http://localhost:3000", "https://fun-code-generator-7jkh.vercel.app/", "https://www.funcodegenerator.me/"],
+    methods: "GET,HEAD,PUT,PATCH,POST",
+    credentials: true,
   })
 );
+
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
