7bf921a118dcaf43c40a72012e598612c0c7976d
Andres Ortiz <63428778+jaortiz16@users.noreply.github.com>
2024-02-04 16:38:55 -0500

    Update app.js

--- funcodegenerator-site/backend/funcodegenerator-api-auth/src/app.js
+++ funcodegenerator-site/backend/funcodegenerator-api-auth/src/app.js
@@ -16,7 +16,13 @@ app.set("json spaces", 4);
 
 // Middlewares
 app.use(
-  cors()
+  cors(
+    {
+    origin: ["http://localhost:3000", "https://fun-code-generator-7jkh.vercel.app/", "https://www.funcodegenerator.me"],
+    methods: "GET,HEAD,PUT,PATCH,POST",
+    credentials: true,
+  }
+  )
 );
 
 // Habilita las solicitudes preflight para todas las rutas
